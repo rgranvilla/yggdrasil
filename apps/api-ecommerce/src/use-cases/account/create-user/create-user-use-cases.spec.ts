0c21c6cf71ff3678b4a315b6911538e2c739ee3b
Ricardo <rgranvilla@gmail.com>
2023-03-12 19:59:43 -0300

    feat(api-ecommerce): first unity test

--- apps/api-ecommerce/src/use-cases/account/create-user/create-user-use-cases.spec.ts
+++ apps/api-ecommerce/src/use-cases/account/create-user/create-user-use-cases.spec.ts
@@ -1,5 +1,36 @@
-import { expect, test } from 'vitest';
+import { describe, expect, it } from 'vitest';
+import { CreateUserUseCase } from './create-user-use-cases';
+import { compare } from 'bcryptjs';
 
-test('check if it works', () => {
-  expect(2 + 2).toBe(4);
+describe('Create User Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const createUserUseCase = new CreateUserUseCase({
+      async findByEmail(email) {
+        return null;
+      },
+
+      async create(data) {
+        return {
+          id: 'user-1',
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        };
+      },
+    });
+
+    const { user } = await createUserUseCase.execute({
+      name: 'John Doe',
+      email: 'johndoe@mail.com',
+      password: '123456',
+    });
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    );
+
+    expect(isPasswordCorrectlyHashed).toBe(true);
+  });
 });
--- apps/api-ecommerce/src/use-cases/account/create-user/create-user-use-cases.ts
+++ apps/api-ecommerce/src/use-cases/account/create-user/create-user-use-cases.ts
@@ -1,5 +1,6 @@
 import { UserAlreadyExistsError } from '@/errors/user-already-exists-error';
 import { UsersRepository } from '@/repositories/users-repository';
+import { User } from '@prisma/client';
 import { hash } from 'bcryptjs';
 
 interface CreateUserUseCaseRequest {
@@ -8,10 +9,18 @@ interface CreateUserUseCaseRequest {
   password: string;
 }
 
+interface CreateUserUseCaseResponse {
+  user: User;
+}
+
 export class CreateUserUseCase {
   constructor(private usersRepository: UsersRepository) {}
 
-  async execute({ name, email, password }: CreateUserUseCaseRequest) {
+  async execute({
+    name,
+    email,
+    password,
+  }: CreateUserUseCaseRequest): Promise<CreateUserUseCaseResponse> {
     const password_hash = await hash(password, 6);
 
     const userWithSameEmail = await this.usersRepository.findByEmail(email);
@@ -20,10 +29,14 @@ export class CreateUserUseCase {
       throw new UserAlreadyExistsError();
     }
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash,
     });
+
+    return {
+      user,
+    };
   }
 }

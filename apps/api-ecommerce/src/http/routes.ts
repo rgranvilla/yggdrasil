92b8c94c53432703a3d3e4a225ac523e12139e07
Ricardo <rgranvilla@gmail.com>
2023-03-12 23:02:15 -0300

    feat(api-ecommerce): add RBCA authorization

--- apps/api-ecommerce/src/http/routes.ts
+++ apps/api-ecommerce/src/http/routes.ts
@@ -1,12 +1,15 @@
 import { FastifyInstance } from 'fastify';
-import { users } from './controllers/users';
-import { authenticate } from './controllers/authenticate';
-import { profile } from './controllers/profile';
+import { authenticate } from './controllers/accounts/authenticate';
+import { profile } from './controllers/accounts/profile';
+import { refresh } from './controllers/accounts/refresh';
+import { users } from './controllers/accounts/users';
 import { verifyJwt } from './middlewares/verify-jwt';
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', users);
   app.post('/sessions', authenticate);
+
+  app.patch('/token/refresh', refresh);
 
   /** Authenticated */
   app.get('/me', { onRequest: [verifyJwt] }, profile);
